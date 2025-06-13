ca4d7ac61236db11b95417a15fdcbdc355b79fc9
Jules Tréhorel <jules.trehorel@iadvize.com>
2025-06-13 11:57:34 +0200

    (fix) fix mobile game layout

--- src/Components/WGOPlayer.jsx
+++ src/Components/WGOPlayer.jsx
@@ -3,10 +3,30 @@ import React, { useEffect, useRef } from 'react';
 export default function WGOPlayer({sgf, move = 0}) {
     const wgoref = useRef(null);
 
+    const layout = [
+        {
+            // Mobile layout
+            conditions: {
+                maxWidth: 980,
+            },
+            layout: {
+                top: ["InfoBox"],
+                bottom: ["Control", "CommentBox"]
+            },
+            className: "wgo-xsmall",
+        },
+        {
+            // Desktop layout
+            layout: window.WGo.BasicPlayer.layouts["right_top"],
+            className: "wgo-twocols wgo-large",
+        }
+    ];
+
     useEffect(() => {
         const elem = wgoref.current;
         new window.WGo.BasicPlayer(elem, {
             sgf: sgf,
+            layout: layout,
             move: parseInt(move)
         });
     }, [sgf])
